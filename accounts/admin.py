94d18079c4bb1ec928050cb5146e98cd07abbd28
KutologoJM <kutologomashego@gmail.com>
2025-10-26 09:27:48 +0800

    OUS-25 Added general functionality to recipe_api app

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.apps import apps
+
 # Register your models here.
 
 
@@ -8,5 +9,5 @@ app = apps.get_app_config("accounts")
 for model_name, model in app.models.items():
     try:
         admin.site.register(model)
-    except admin.sites.AlreadyRegistered:
+    except admin.sites.AlreadyRegistered:  # noqa
         pass
--- recipe_api/admin.py
+++ recipe_api/admin.py
@@ -1,12 +1,11 @@
-from django.apps import apps
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
 
-app = apps.get_app_config("recipe_api")
-
-for model_name, model in app.models.items():
-    try:
-        admin.site.register(model)
-    except admin.sites.AlreadyRegistered:
-        pass
+admin.site.register(IngredientsModel)
+admin.site.register(RecipeModel)
+admin.site.register(SourcesModel)
+admin.site.register(RecipeIngredientsModel)
+admin.site.register(RecipeSourcesModel)
+admin.site.register(IngredientSourcesModel)
