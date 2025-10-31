f163f8079c825fded36341acec9018dbd2317380
Kutologo J. Mashego <kutologomashego@gmail.com>
2025-10-31 14:09:35 +0800

    Update settings.py

--- ONI_Utility_Suite/settings.py
+++ ONI_Utility_Suite/settings.py
@@ -191,3 +191,7 @@ REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
 }
+
+# Proxy Settings
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
